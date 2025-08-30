e599ecc4a88901b535049610aeab44d61e6d13ce
Fox-hosting <omarmagames43@gmail.com>
2025-08-31 01:58:18 +0300

    Update Ngrok.py

--- Ngrok.py
+++ Ngrok.py
@@ -5,13 +5,13 @@ print("✅ بدأ تشغيل سكريبت ngrok الخاص بفوكس...")
 # 1️⃣ أمر التثبيت الكامل
 os.system('curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
-  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" \
   | sudo tee /etc/apt/sources.list.d/ngrok.list \
   && sudo apt update \
   && sudo apt install ngrok')
 
 # 2️⃣ أمر إضافة التوكن
-os.system('ngrok config add-authtoken 2yQ2EUONyEin5FFL0p1zvxrpde8_6w4wxjKdAcWoSHMAWkJNa')
+os.system('ngrok config add-authtoken 2xtqSN8iOOHUZpfOIselkoJz5Da_cyRogaN8y71DsxWofND5')
 
 # 3️⃣ أمر تشغيل النفق
 os.system('ngrok http http://localhost:443')
