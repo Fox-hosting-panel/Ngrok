e75d4d823b48a2a0a438e1dd59463f70575139f2
Fox-hosting <omarmagames43@gmail.com>
2026-08-11 06:57:05 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,11 @@
-#!/bin/bash
-
-# تأكد إن السكريبت شغال بصلاحيات root
-if [ "$EUID" -ne 0 ]; then
-  echo "من فضلك شغل السكريبت باستخدام sudo"
-  exit
-fi
-
-echo "🛠️ تحديث الحزم..."
 sudo su
 
-echo "⬆️ ترقية النظام..."
 sudo apt update
 
-echo "🐳 تثبيت Docker Compose..."
 sudo apt upgrade
 
-echo "🖥️ تثبيت Neofetch..."
 apt install docker-compose -y
 
-echo "📊 تشغيل Neofetch..."
 apt install neofetch
 
-echo "📊 تشغيل Neofetch..."
 neofetch
